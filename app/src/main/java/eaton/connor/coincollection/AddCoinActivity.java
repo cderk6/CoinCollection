ebc4149bf698d7c2b8807be9b36bd6840049396f
cderk6 <cderk6@mail.umkc.edu>
2018-02-19 02:40:42 -0600

    Coin entry UI

--- app/src/main/java/eaton/connor/coincollection/AddCoinActivity.java
+++ app/src/main/java/eaton/connor/coincollection/AddCoinActivity.java
@@ -1,7 +1,16 @@
 package eaton.connor.coincollection;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AddCoinActivity extends AppCompatActivity {
 
@@ -9,5 +18,71 @@ public class AddCoinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_coin);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        // Populate these from db later
+        String[] array_denom = new String[] {"$1"};
+        String[] array_type = new String[] {"Morgan Dollar"};
+        String[] array_year = new String[] {"1879"};
+        String[] array_mint = new String[] {"O"};
+        String[] array_grade = new String[] {"MS63"};
+
+        Spinner spinner_denom = (Spinner) findViewById(R.id.spinner_denomination);
+        ArrayAdapter<String> adapter_denom = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, array_denom);
+        adapter_denom.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_denom.setAdapter(adapter_denom);
+
+        Spinner spinner_type = (Spinner) findViewById(R.id.spinner_type);
+        ArrayAdapter<String> adapter_type = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, array_type);
+        adapter_type.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_type.setAdapter(adapter_type);
+
+        Spinner spinner_year = (Spinner) findViewById(R.id.spinner_year);
+        ArrayAdapter<String> adapter_year = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, array_year);
+        adapter_year.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_year.setAdapter(adapter_year);
+
+        Spinner spinner_mint = (Spinner) findViewById(R.id.spinner_mint);
+        ArrayAdapter<String> adapter_mint = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, array_mint);
+        adapter_mint.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_mint.setAdapter(adapter_mint);
+
+        Spinner spinner_grade = (Spinner) findViewById(R.id.spinner_grade);
+        ArrayAdapter<String> adapter_grade = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, array_grade);
+        adapter_grade.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_grade.setAdapter(adapter_grade);
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_coin_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add_coin:
+                // Submit coin data to db
+                return true;
+            case R.id.action_cancel:
+                // Discard data. Switch to HomeActivity
+                startActivity(new Intent(AddCoinActivity.this, HomeActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
