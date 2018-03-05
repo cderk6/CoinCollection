a1d4a980de2299f88914554917f2b5b5d6dbfc12
cderk6 <cderk6@mail.umkc.edu>
2018-03-04 23:47:34 -0600

    Sprint#2 completed. Sprint#3 planned

--- app/src/main/java/eaton/connor/coincollection/ScanActivity.java
+++ app/src/main/java/eaton/connor/coincollection/ScanActivity.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// Modifications by Connor Eaton, 2018
 package eaton.connor.coincollection;
 
 import android.Manifest;
