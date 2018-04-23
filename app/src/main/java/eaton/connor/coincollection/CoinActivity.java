0c8e751924a893b1094fc81beb74baaee5cf5b9f
cderk6 <cderk6@mail.umkc.edu>
2018-04-23 01:18:11 -0500

    Option to delete coins from database and the corresponding images

--- app/src/main/java/eaton/connor/coincollection/CoinActivity.java
+++ app/src/main/java/eaton/connor/coincollection/CoinActivity.java
@@ -1,5 +1,6 @@
 package eaton.connor.coincollection;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.Image;
 import android.support.annotation.NonNull;
@@ -7,6 +8,7 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -18,11 +20,17 @@ import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -40,6 +48,7 @@ public class CoinActivity extends AppCompatActivity {
     TableLayout tableLayout;
 
     StorageReference mStorage;
+    FirebaseFirestore db;
 
     private DrawerLayout mDrawer;
     private Toolbar toolbar;
@@ -52,20 +61,22 @@ public class CoinActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coin);
 
+        db = FirebaseFirestore.getInstance();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerToggle = new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
+        drawerToggle = new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open, R.string.drawer_close);
         mDrawer.addDrawerListener(drawerToggle);
 
-        nvDrawer =(NavigationView) findViewById(R.id.nvView);
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
 
         // Init click listeners for nav
         nvDrawer.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch(item.getItemId()){
+                        switch (item.getItemId()) {
                             case R.id.nav_home:
                                 // switch to home activity/fragment
                                 break;
@@ -102,16 +113,16 @@ public class CoinActivity extends AppCompatActivity {
         buildTable();
     }
 
-    private void buildTable(){
+    private void buildTable() {
         Map<String, String> coin_map = coin.getMap();
         String[] picOrder = {"Obverse", "Reverse"};
         String[] tableOrder = {"year", "mint", "series", "grade", "denomination", "price", "barcode"};
         String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-        for (int i = 0; i < picOrder.length; i++){
+        for (int i = 0; i < picOrder.length; i++) {
             TextView c1 = new TextView(this);
             c1.setText(picOrder[i].toUpperCase());
-            final StorageReference ref= mStorage.child("Users").child(uid).child(picOrder[i] + "s").child(coin_id);
+            final StorageReference ref = mStorage.child("Users").child(uid).child(picOrder[i] + "s").child(coin_id);
             ImageView img = new ImageView(this);
             GlideApp.with(this).load(ref).into(img);
             img.setOnClickListener(new View.OnClickListener() {
@@ -136,7 +147,7 @@ public class CoinActivity extends AppCompatActivity {
         }
 
 
-        for(int i = 0; i < tableOrder.length; i++) {
+        for (int i = 0; i < tableOrder.length; i++) {
             TableRow row = new TableRow(this);
             TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
             row.setLayoutParams(lp);
@@ -150,7 +161,8 @@ public class CoinActivity extends AppCompatActivity {
         }
 
     }
-    private void signOut(){
+
+    private void signOut() {
         AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
@@ -160,13 +172,21 @@ public class CoinActivity extends AppCompatActivity {
             }
         });
     }
+
     @Override
     public void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         drawerToggle.syncState();
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        final DocumentReference ref = db.collection("users").document(uid).collection("coins").document(coin_id);
+        final StorageReference ref_img_obv = mStorage.child("Users").child(uid).child("Obverses").child(coin_id);
+        final StorageReference ref_img_rev = mStorage.child("Users").child(uid).child("Reverses").child(coin_id);
+
+
         // The action bar home/up action should open or close the drawer.
         switch (item.getItemId()) {
             case android.R.id.home:
@@ -176,7 +196,42 @@ public class CoinActivity extends AppCompatActivity {
                 // Open edit activity
                 return true;
             case R.id.action_delete:
-                //
+                // Delete coin from Firestore db and corresponding images from Firebase Storage
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                alertDialogBuilder.setTitle("Confirm delete");
+                alertDialogBuilder.setMessage("Are you sure you want to delete this coin?");
+                alertDialogBuilder.setPositiveButton("Delete",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                ref.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(CoinActivity.this, "Coin successfully deleted", Toast.LENGTH_LONG).show();
+                                    }
+                                })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(CoinActivity.this, "Error deleting coin", Toast.LENGTH_LONG).show();
+                                            }
+                                        });
+                                ref_img_obv.delete();
+                                ref_img_rev.delete();
+                                startActivity(new Intent(CoinActivity.this, HomeActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                                finish();
+                            }
+                        });
+
+                alertDialogBuilder.setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
