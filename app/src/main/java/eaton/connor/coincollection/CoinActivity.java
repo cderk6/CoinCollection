75a7734c7f96642e56b6464851aae411086baa8c
cderk6 <cderk6@mail.umkc.edu>
2018-04-22 22:50:40 -0500

    Edit and delete menu buttons added to UI of coin activity

--- app/src/main/java/eaton/connor/coincollection/CoinActivity.java
+++ app/src/main/java/eaton/connor/coincollection/CoinActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -170,8 +172,22 @@ public class CoinActivity extends AppCompatActivity {
             case android.R.id.home:
                 mDrawer.openDrawer(GravityCompat.START);
                 return true;
+            case R.id.action_edit:
+                // Open edit activity
+                return true;
+            case R.id.action_delete:
+                //
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.edit_coin_menu, menu);
+        return true;
+    }
+
 }
